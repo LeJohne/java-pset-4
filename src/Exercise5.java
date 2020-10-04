33bc8a09957dda440c4912dc44fa52e078d85ea4
LeJohne <johngoy11024@gmail.com>
2020-10-04 19:39:40 -0400

    Exercise5 - finished

---
+++ src/Exercise5.java
@@ -0,0 +1,19 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        DecimalFormat deci = new DecimalFormat("###,###,###,###.00");
+
+        System.out.print("Diameter : ");
+        double D = in.nextDouble();
+
+        double R = D / 2;
+        double CArea = Math.pow(R, 2) * Math.PI;
+        double CCircum = 2 * Math.PI * R;
+
+        System.out.println("\nArea          : " + deci.format(CArea) + "\nCircumference : " + deci.format(CCircum));
+    }
+}
